f2e66a28a09f06a82ea45ca16db7e1a852edac03
Chris Chiquet <chiquet.chris@gmail.com>
2021-12-01 14:08:43 -0800

    more work

--- infinite_monkey/src/App.js
+++ infinite_monkey/src/App.js
@@ -13,18 +13,18 @@ function App() {
           
         </div>
         <div className="RightNav">
-          // connect wallet button
+          <button className="ConnectBtn">connect wallet</button>
         </div>
       </div>
       <ProSidebar>
-        <Menu iconShape="circle">
-          <MenuItem icon={<Icon.Book />}>Book</MenuItem>
-          <SubMenu title="Team" icon={<Icon.Diamond />}>
+        <Menu iconShape="circle" className="MainSidebar">
+          <MenuItem className="SidebarButton" icon={<Icon.Book />} icon-background-color="blue">Book</MenuItem>
+          <SubMenu className="SidebarButton" title="Team" icon={<Icon.PeopleFill />}>
             <MenuItem>Component 1</MenuItem>
             <MenuItem>Component 2</MenuItem>
           </SubMenu>
-          <MenuItem icon={<Icon.Check />}>Vote</MenuItem>
-          <MenuItem icon={<Icon.CurrencyDollar />}>Buy</MenuItem>  
+          <MenuItem className="SidebarButton" icon={<Icon.Check />}>Vote</MenuItem>
+          <MenuItem className="SidebarButton" icon={<Icon.CurrencyDollar />}>Buy</MenuItem>  
         </Menu>
       </ProSidebar>
     </div>
