cf86dbbf4daefc3242c7f35239fb6ee4763ebe6d
Chris Chiquet <chiquet.chris@gmail.com>
2021-12-01 16:59:19 -0800

    Move to native sidebar

--- infinite_monkey/src/App.js
+++ infinite_monkey/src/App.js
@@ -1,32 +1,13 @@
 import logo from './logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import * as Icon from 'react-bootstrap-icons';
-import 'react-pro-sidebar/dist/css/styles.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="TopNav">
-        <div className="LeftNav">
-          
-        </div>
-        <div className="RightNav">
-          <button className="ConnectBtn">connect wallet</button>
-        </div>
-      </div>
-      <ProSidebar>
-        <Menu iconShape="circle" className="MainSidebar">
-          <MenuItem className="SidebarButton" icon={<Icon.Book />} icon-background-color="blue">Book</MenuItem>
-          <SubMenu className="SidebarButton" title="Team" icon={<Icon.PeopleFill />}>
-            <MenuItem>Component 1</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-          </SubMenu>
-          <MenuItem className="SidebarButton" icon={<Icon.Check />}>Vote</MenuItem>
-          <MenuItem className="SidebarButton" icon={<Icon.CurrencyDollar />}>Buy</MenuItem>  
-        </Menu>
-      </ProSidebar>
+      <Router>
+          <Route path="/" element={<Book />} ></Route>
+      </Router>
     </div>
   );
 }
---
+++ infinite_monkey/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import * as FaIcons from 'react-icons/fa';
+
+function Navbar() {
+    <div className="navbar">
+        <Link to="#" className="menu-bars">
+            <FaIcons.FaBars/>
+        </Link>
+    </div>
+}
+
+export default Navbar
