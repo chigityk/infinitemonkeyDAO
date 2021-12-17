f22c4bc612fc9cf62d460a91b83080ae36c9f632
Chris Chiquet <chiquet.chris@gmail.com>
2021-12-17 14:14:27 -0800

    adjustments

--- infinite_monkey/src/pages/Book.js
+++ infinite_monkey/src/pages/Book.js
@@ -1,5 +1,19 @@
 const Book = () => {
-    return <h1>THE LOOM</h1>;
+    return (
+      <>
+      <h1>THE LOOM</h1>
+      <div className="loom">
+        <p>book</p>
+      </div>
+      <div className="loomfoot">
+        <p>
+          The content of The LOOM is 100% community-voted. <br></br>
+          No words or punctuation are placed without at a majority vote.
+        </p>
+      </div>
+      
+      </>
+    )
   };
   
   export default Book;
--- infinite_monkey/src/pages/NoPage.js
+++ infinite_monkey/src/pages/NoPage.js
@@ -1,5 +1,12 @@
 const NoPage = () => {
-    return <h1>404</h1>;
+    return (
+      <>
+      <div className="errorcontain">
+        <h1>404</h1>
+        <p>Sorry, our monkeys haven't written that part yet.</p>
+      </div>
+      </>
+    )
   };
   
   export default NoPage;
